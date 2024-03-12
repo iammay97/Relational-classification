158ed874b1595fde734a5d09d8279c4138d1cf8f
iammay97 <1275970752@qq.com>
2024-03-12 13:43:26 +0800

    “Modify comments”

--- data/relation_bar_chart.py
+++ data/relation_bar_chart.py
@@ -17,7 +17,8 @@ plt.rcParams['axes.unicode_minus']=False
 
 # Plotting Bar Graphs with the Matplotlib Module
 x = range(len(num_list))
-rects = plt.bar(x=x, height=num_list, width=0.6, color='blue', label="频数")
+rects = plt.bar(x=x, height=num_list, width=0.6, color='blue', label="count")
+
 # plt.ylim(0, 800) # y-axis range
 plt.ylabel("amount")
 plt.xticks([index + 0.1 for index in x], label_list)
@@ -31,17 +32,19 @@ for rect in rects:
     height = rect.get_height()
     plt.text(rect.get_x() + rect.get_width() / 2, height+1, str(height), ha="center", va="bottom")
 
-# plt.show()
+#plt.show()
 plt.savefig('./bar_chart.png')
 
 
 plt.figure(figsize = (10, 4))
 
+'''
 plt.subplot(1, 3, 1)
 plt.title("Training Set")
-sns.histplot(train, x='length',hue='label',kde=True)
+sns.histplot('train', x='length',hue='label',kde=True)
 
 plt.subplot(1, 3, 3)
 plt.title("Testing Set")
-sns.histplot(test,x='length',hue='label',kde=True)
+sns.histplot('test',x='length',hue='label',kde=True)
 plt.show()
+'''
